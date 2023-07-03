5f056bdaa7a1f1ab4f0a3dcbc2dfe615e4de1038
Yunse LEE <yunselee@gmail.com>
2023-07-03 16:28:14 +0200

    feat: add route and input box

--- src/routes/asd/+page.svelte
+++ src/routes/asd/+page.svelte
@@ -1,16 +1,5 @@
 <div id="bab">
-    <h1 class="phrase">
-        <div class="phrase-text quote">
-            "인생은 낮저밤이"
-        </div>
-        <div class="phrase-text">
-            낮에는 저거 먹고 밤에는 이거 먹고
-        </div>
-    </h1>
-    <div class="food-input">
-        <input>
-        <button>추가</button>
-    </div>
+    asdasd
 </div>
 
 <style lang="scss" scoped>
