2fedda53137609dca604642b058f251d10228593
Yunse LEE <yunselee@gmail.com>
2023-07-03 16:05:52 +0200

    feat: add simple responsive to phrase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,30 @@
 <div id="bab">
     <h1 class="phrase">
-        <div class="quote">
+        <div class="phrase-text quote">
             "인생은 낮저밤이"
         </div>
-        <div class="text">
+        <div class="phrase-text">
             낮에는 저거 먹고 밤에는 이거 먹고
         </div>
     </h1>
+    <div>
+        asdasd
+    </div>
 </div>
 
 <style lang="scss" scoped>
 #bab {
-    display: flex;
-    justify-content: center;
     .phrase {
+        @media (max-width: 640px){
+            font-size: medium;
+        }
+        .phrase-text {
+            display: flex;
+            justify-content: center;
+        }
         .quote {
-            display: flex;
-            justify-content: center;
         }
-        .text {
-            display: flex;
-            justify-content: center;
-        }
+
     }
 }
 </style>
