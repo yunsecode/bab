b26a54e8bf94150d0decc10274dcf23e539ad3e5
Yunse LEE <yunselee@gmail.com>
2023-07-04 11:13:12 +0200

    feat: tmp for arc

---
+++ src/components/FullArc.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let cx=0, cy=0, r
+
+
+    let arcSweep = 1;
+
+    let d = `
+        M ${cx + r} ${cy}
+        A ${r} ${r} 0 ${arcSweep} 0 ${cx - r} ${cy}
+        A ${r} ${r} 0 ${arcSweep} 0 ${cx + r} ${cy}
+    `;
+</script>
+
+<path {d}/>
+
+<style>
+    path {
+      fill: green;
+      stroke-width: 2;
+      stroke: #fff;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,44 @@
         </div>
     </h1>
     <div class="food-input">
-        <input>
-        <button>추가</button>
+        <input bind:value={input_value}>
+        <button on:click={() => onClickTopic()}>추가</button>
     </div>
+
+    <div>
+        asd {asd} <br/>
+        oprions {options} <br/>
+        aaa{options.length}
+        <svg height="400" width="400">
+            bbb{options.length}
+            <g transform="translate(200,200)">
+              ccc{options.length}
+            <RouletteWheel
+              r={150}
+              options={options}
+              onRolled={onRolled}
+            />
+          </g>
+          <polygon points="200 360 210 370 190 370"/>
+        </svg>
+      </div>
 </div>
+
+<script>
+    import RouletteWheel from "../components/RouletteWheel.svelte"
+    let options = [];
+    let asd = 0;
+    let input_value = 0;
+
+    function onRolled(opt) {
+        console.log(opt)
+    }
+    function onClickTopic() {
+        options = [...options, asd++];
+        asd++;
+        console.log(111);
+    }
+</script>
 
 <style lang="scss" scoped>
 #bab {
@@ -32,5 +66,23 @@
         justify-content: center;
         gap: 20px;
     }
+}
+:global(body) {
+  background-color: #444;
+  color: #fff;
+  margin: 0;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  user-select: none;
+}
+
+svg {
+  display: block;
+}
+
+polygon {
+  fill: yellow;
 }
 </style>
